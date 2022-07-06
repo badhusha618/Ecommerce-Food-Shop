dc3d160f9aa0b7e033082f93053f021141c7f2b3
Shahul Hameed Badhusha <shahul@makinus.com>
2022-07-06 19:58:10 +0530

    update on product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -6,7 +7,7 @@ import { ProductComponent } from './product/product.component';
 
 const appRoutes: Routes = [
   { path: '', component: ProductListComponent },
-  { path: 'product', component: ProductComponent }
+  { path: 'product', component: ProductDetailsComponent }
 ];
 @NgModule({
   declarations: [],
